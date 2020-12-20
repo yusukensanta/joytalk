c1468f1f444be139e7a322bf853e57a543f6f50a
yusukensanta <yusuke.nakayama1218@gmail.com>
2020-12-20 21:34:28 +0900

    feat: post message in case just leave channel

--- joytalk.py
+++ joytalk.py
@@ -66,7 +66,7 @@ class JoyTalk(discord.Client):
                     )
 
                 if voice_state:
-                    self.CHATS[message.guild.id] = message.channel.id
+                    self.CHATS[message.guild.id] = message.channel
                     self.VOICE_CLIENTS[
                         message.guild.id] = await voice_state.channel.connect(
                         )
@@ -87,12 +87,12 @@ class JoyTalk(discord.Client):
                 await message.channel.send("β版なためこのサーバーではJoyTalkは使えません")
             elif self.VOICE_CLIENTS.get(message.guild.id, None):
                 await self.VOICE_CLIENTS[message.guild.id].disconnect()
-                await message.channel.send("お疲れ様でした")
+                await message.channel.send("お疲れ様でした:thumbsup:")
 
         elif voice_state and self.VOICE_CLIENTS.get(
                 message.guild.id, None) and self.CHATS.get(
                     message.guild.id, None) and self.CHATS.get(
-                        message.guild.id) == message.channel.id:
+                        message.guild.id).id == message.channel.id:
             voice_path = self._generate_audio(message.content)
             self._play(voice_path, self.VOICE_CLIENTS[message.guild.id])
 
@@ -103,6 +103,7 @@ class JoyTalk(discord.Client):
                 member.guild.id].channel.id:
             vc = self.VOICE_CLIENTS[member.guild.id]
             self.VOICE_CLIENTS[member.guild.id] = None
+            await self.CHATS[member.guild.id].send("お疲れ様でした:video_game:")
             self.CHATS[member.guild.id] = None
             try:
                 await vc.disconnect()
